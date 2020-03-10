4ec66bf4e4cef53bd961faa3e25867d58b5669ba
ClassicVantage <61285209+ClassicVantage@users.noreply.github.com>
2020-03-10 21:18:46 +0530

    c24

---
+++ piggy.js
@@ -0,0 +1,27 @@
+class Pig{
+    constructor (x,y){
+        var options = {
+        restituition:0.8 ,
+         friction: 0.3,
+         density:1.0,
+        }
+        this.body=Bodies.rectangle(x,y,50,50,options);
+        this.width=50;
+        this.height=50;
+
+        World.add(world,this.body);
+    }
+display (){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(0);
+    strokeWeight(8);
+    stroke("limegreen");
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+}
